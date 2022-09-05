00eb261b3f5f55de617803ec450bcc6f9d17714e
kimgeonil <kji8922@naver.com>
2022-09-05 11:47:21 +0900

    Add useState in react module

--- src/useState/useState.js
+++ src/useState/useState.js
@@ -1,15 +1,16 @@
 // type SetStateAction<s> = s | ((prevState: s) =>s);
 // type Dispatch<A> = (value: A) => void;
 // function useState<s>(initialState: s | (() => s)): [s, Dispatch<SetStateAction<s>>];
+// state를 변수로 선언했더니 useState에서 return한 순간 변경할 수 없는 상태가 되서 setState가 이루어지지 않는다.
 function useState(initialState) {
     var value = initialState;
-    var state = function () { return value; };
+    // const state = () => value;
     var setState = function (newState) {
         value = newState;
     };
-    return [state, setState];
+    return [value, setState];
 }
 var _a = useState(0), number = _a[0], setNumber = _a[1];
-console.log(number());
+console.log(number);
 setNumber(1);
-console.log(number());
+console.log(number);
