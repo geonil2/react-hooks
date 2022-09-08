09ba6ee7b127bcef5182446ac13a89203a0e8cc5
kimgeonil <kji8922@naver.com>
2022-09-08 14:15:43 +0900

    Update useState

--- src/useState/useState.ts
+++ src/useState/useState.ts
@@ -1,6 +1,19 @@
+let states: any = [];
+let cursor: number = 0;
 
 function useState<S>(initialState: S) {
+    if (states.length === cursor) {
+        states.push(initialState);
+    }
 
+    const state = states[cursor];
+    function setState(newState: S) {
+        state[cursor] = newState;
+        render();
+    }
+
+    cursor++;
+    return [state, setState];
 }
 
 function app() {
@@ -9,15 +22,21 @@ function app() {
 }
 
 function myComponent() {
-    const [number, setNumber] = myUseState(1);
+    const [count, setCount] = useState(1);
+
+    window['increment'] = () => setCount(count + 1);
+
     return `
         <div>
-          <button>hello</button>
+          <strong>count: ${count} </strong>
+          <button onclick="increment()">증가</button>
         </div>
-    `;
+      `;
 }
 
 function render() {
-    const root = document.querySelector('#root');
+    const root = document.querySelector('#root')!;
     root.innerHTML = app();
 }
+
+render();
